8fac5fd64974b08f993483dbbebfd81579b6dd8c
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-11-14 23:32:32 +0900

    [ADD] 토큰 유효 시간 1일

--- src/main/java/com/example/OpenSource/domain/auth/jwt/TokenProvider.java
+++ src/main/java/com/example/OpenSource/domain/auth/jwt/TokenProvider.java
@@ -1,9 +1,21 @@
 package com.example.OpenSource.domain.auth.jwt;
 
 import com.example.OpenSource.domain.auth.dto.TokenDto;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,19 +26,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.stream.Collectors;
-
 @Slf4j
 @Component
 public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
     //private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30; //30분
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60; //30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24; // 1일
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7; //7일
 
     private final Key key;
@@ -37,7 +43,7 @@ public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
     }
 
     //유저 정보를 넘겨받아서 토큰을 생성하는 메서드
-    public TokenDto generateTokenDto(Authentication authentication){
+    public TokenDto generateTokenDto(Authentication authentication) {
         //권한들 가져오기
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
@@ -75,7 +81,7 @@ public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
         - 비밀번호 체크를 한 객체 authentication 이 없음.
         - 그래서 매개변수가 다른 같은 동작을 하는 함수로 오버로딩함..
     */
-    public TokenDto generateTokenDto(Long memberId){
+    public TokenDto generateTokenDto(Long memberId) {
         long now = (new Date()).getTime();
 
         String subject = memberId.toString();
@@ -103,11 +109,11 @@ public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
     }
 
     //JWT 토큰을 복호화하여 토큰에 들어있는 정보를 꺼내는 메서드
-    public Authentication getAuthentication(String accessToken){
+    public Authentication getAuthentication(String accessToken) {
         //토큰 복호화 - accessToken 에만 user 정보가 있기에
         Claims claims = parseClaims(accessToken);
 
-        if(claims.get(AUTHORITIES_KEY) == null){
+        if (claims.get(AUTHORITIES_KEY) == null) {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
 
@@ -118,9 +124,9 @@ public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
                         .collect(Collectors.toList());
 
         //UserDetails 객체를 만들어서 Authentication 리턴
-        UserDetails principal = new User(claims.getSubject(),"", authorities);
+        UserDetails principal = new User(claims.getSubject(), "", authorities);
 
-        return new UsernamePasswordAuthenticationToken(principal,"",authorities);
+        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 
     //만료된 토큰이어도 정보를 꺼내기 위해 분리
@@ -134,16 +140,16 @@ public class TokenProvider { //JWT 토큰에 관련된 암호화, 검증 로직
 
     // 토큰을 검증하기 위한 메서드
     public boolean validateToken(String token) {
-        try{ //Jwts 모듈이 알아서 Exception을 던져줌
+        try { //Jwts 모듈이 알아서 Exception을 던져줌
             Jws<Claims> claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
-        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e){
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
             log.info("잘못된 JWT 서명입니다.");
             throw new JwtException("잘못된 JWT 서명입니다.");
-        } catch (ExpiredJwtException e){
+        } catch (ExpiredJwtException e) {
             log.info("만료된 JWT 토큰입니다.");
             throw new JwtException("만료된 JWT 토큰입니다.");
-        } catch (UnsupportedJwtException e){
+        } catch (UnsupportedJwtException e) {
             log.info("JWT 토큰이 잘못되었습니다.");
             throw new JwtException("JWT 토큰이 잘못되었습니다.");
         }
