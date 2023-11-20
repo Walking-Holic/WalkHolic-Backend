8976b60dd52535fdba78be33d2cf0c39cb184fc8
lunghyun <103187357+lunghyun@users.noreply.github.com>
2023-11-21 02:08:59 +0900

    DTO 오류 수정

--- src/main/java/com/example/OpenSource/domain/path/dto/CoordinateDto.java
+++ src/main/java/com/example/OpenSource/domain/path/dto/CoordinateDto.java
@@ -1,12 +1,21 @@
 package com.example.OpenSource.domain.path.dto;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CoordinateDto {
     private int sequence;
     private double latitude;
     private double longitude;
+
+    @Builder
+    public CoordinateDto(int sequence, double latitude, double longitude) {
+        this.sequence = sequence;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
