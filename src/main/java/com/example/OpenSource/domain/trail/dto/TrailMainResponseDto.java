0b13e423a51ab988e6ea918953b4e1ebabd46a73
kimgunwooo <96743351+kimgunwooo@users.noreply.github.com>
2023-12-06 01:47:00 +0900

    평점, 댓글 수 출력

--- src/main/java/com/example/OpenSource/domain/trail/dto/TrailMainResponseDto.java
+++ src/main/java/com/example/OpenSource/domain/trail/dto/TrailMainResponseDto.java
@@ -10,11 +10,14 @@ public class TrailMainResponseDto {
     private Long id;
     private String wlkCoursFlagNm; // 산책 경로 구분명
     private String wlkCoursNm; // 산책 경로명
-    // TODO: 댓글 테이블과 연결해서 평점과 댓글 수 출력
+    private double averageScore;
+    private int commentCount;
 
     public TrailMainResponseDto(Trail trail) {
         this.id = trail.getId();
         this.wlkCoursFlagNm = trail.getWlkCoursFlagNm();
         this.wlkCoursNm = trail.getWlkCoursNm();
+        this.averageScore = trail.getAverageScore();
+        this.commentCount = trail.getComments().size();
     }
 }
